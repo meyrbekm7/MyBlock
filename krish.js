cc476f0cde8d6bb8411621a519fabcb16ef4c822
meyrbek <mikomavvliyankulov@gamil.com>
2026-03-02 19:14:02 +0500

    all

--- krish.js
+++ krish.js
@@ -2,29 +2,16 @@ let ism = document.getElementById('name');
 let family = document.getElementById('family');
 
 
-function tekshir() {
-
-    let ismUzunligi = ism.value.length; 
-
-    if (ismUzunligi >= 10) {
-        alert('Ismingiz juda kop')
-   
-    } 
-    else if (ismUzunligi <= 2) {
-        alert('Ismingiz jua kam')
-        
-      
-    } 
-
-    let FamilyUzun=family.value.length;
-
-    if(FamilyUzun>15){
-        alert('familyangiz Juda kop')
-    }else if(FamilyUzun<5){
-        alert('familyangiz juda kam')
+function tekshir(){
+    if(ism.value ===''|| family.value === ''){
+        alert('iltimos Ism va familya kriting');
+        return false
     }
+    return true;
+
+
     
-  
 }
+
 
 
